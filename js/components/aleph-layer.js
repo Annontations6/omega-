630738ee8d0bb2183a67e05530ee569c39580c8d
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-06-18 09:42:45 +0100

    Update aleph-layer.js

--- js/components/aleph-layer.js
+++ js/components/aleph-layer.js
@@ -8,7 +8,7 @@ Vue.component("aleph-layer", {
     computed: {
         canProduceAleph: function()
         {
-            return this.aleph.getAlephBoostFromLayer().gt(0);
+            return game.achievements[3].isCompleted == true;
         },
         isSoftCapped: function()
         {
@@ -24,14 +24,6 @@ Vue.component("aleph-layer", {
     <p>You have {{formatNumber(aleph.aleph, 2, 2, 1e9)}} <span class="aleph">ℵ</span></p>
     <p>You get {{formatNumber(aleph.getAlephGain(), 2, 2, 1e9)}} <span class="aleph">ℵ</span> every second</p>
 </div>
-<div class="boosts">
-    <div v-if="canProduceAleph">
-        <p>Your highest Layer is <resource-name :layerid="highestLayer()"></resource-name>, translated to a x{{formatNumber(aleph.getAlephBoostFromLayer(), 2, 2)}} Boost on <span class="aleph">ℵ</span> gain</p>
-    </div>
-    <div v-else>
-        <p>You need to go <resource-name :layerid="3"></resource-name> at least once to get <span class="aleph">ℵ</span></p>
-    </div>
-</div>
 <div class="tabs">
     <button @click="aleph.maxAll()">Max All (M)</button>
 </div>
@@ -41,14 +33,7 @@ Vue.component("aleph-layer", {
 </div>
 <h3>Enhancers</h3>
 <div class="upgrades">
-    <aleph-upgrade :upgrade="aleph.upgrades.deltaBoost"></aleph-upgrade>
-    <aleph-upgrade :upgrade="aleph.upgrades.alephBoost"></aleph-upgrade>
-    <aleph-upgrade :upgrade="aleph.upgrades.powerGenerators"></aleph-upgrade>
-    <aleph-upgrade :upgrade="aleph.upgrades.prestigeNoPowerBoost"></aleph-upgrade>
-    <aleph-upgrade :upgrade="aleph.upgrades.betterBetaFormula"></aleph-upgrade>
-    <aleph-upgrade :upgrade="aleph.upgrades.alephBoost2"></aleph-upgrade>
-    <aleph-upgrade :upgrade="aleph.upgrades.prestigeRewards"></aleph-upgrade>
-    <aleph-upgrade :upgrade="aleph.upgrades.layerExponentialBoost"></aleph-upgrade>
+    coming soon
 </div>
 </div>`
 });
